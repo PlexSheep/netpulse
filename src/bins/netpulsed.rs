5e8f0c4a9e36db6e1b54db38ebe250e54ff26a11
PlexSheep <software@cscherr.de>
2024-11-07 15:02:48 +0100

    feat(daemon): first daemon app with made up checks

--- src/bins/netpulsed.rs
+++ src/bins/netpulsed.rs
@@ -1,3 +1,53 @@
+use std::fs::{self, File};
+use std::time::{self, Duration};
+
+use daemonize::Daemonize;
+use netpulse::records::{Check, CheckFlag};
+use netpulse::store::Store;
+use netpulse::{DAEMON_LOG_ERR, DAEMON_LOG_INF};
+
 fn main() {
-    todo!()
+    let path = Store::path();
+    let parent_path = path.parent().expect("store file has no parent directory");
+    println!("Parent: {parent_path:?}");
+
+    let logfile = File::create(DAEMON_LOG_INF).expect("could not open info logfile");
+    let errfile = File::create(DAEMON_LOG_ERR).expect("could not open error logfile");
+
+    fs::create_dir_all(parent_path).expect("could not create the store directory");
+
+    let daemonize = Daemonize::new()
+        .pid_file("/run/netpulse.pid")
+        .chown_pid_file(true)
+        .working_directory(parent_path)
+        .user("netpulse")
+        .group("netpulse")
+        .stdout(logfile)
+        .stderr(errfile)
+        .umask(0o027); // rwxr-x---
+
+    eprintln!("daemon setup done");
+
+    match daemonize.start() {
+        Ok(_) => daemon(),
+        Err(e) => {
+            eprintln!("Error starting daemon: {:#}", e);
+        }
+    }
+
+    eprintln!("end?")
+}
+
+fn daemon() {
+    println!("starting daemon...");
+    let mut store = Store::load_or_create().expect("boom");
+    loop {
+        store.add_check(Check::new(
+            time::SystemTime::now(),
+            CheckFlag::Success,
+            None,
+        ));
+        store.save().expect("could not save");
+        std::thread::sleep(Duration::from_secs(5));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 /// How long to wait until considering a connection as timed out, in milliseconds
 pub const TIMEOUT_MS: u16 = 30_000;
 
+pub const DAEMON_PID_FILE: &str = "/run/netpulse.pid";
+pub const DAEMON_LOG_ERR: &str = "/var/log/netpulse.err";
+pub const DAEMON_LOG_INF: &str = "/var/log/netpulse.log";
+
 pub mod errors;
 pub mod records;
 pub mod store;
